39520c29124722e377a42e8e3032c44ff505523f
Ashutosh Dubey <idkashutosh@gmail.com>
2021-04-07 18:54:49 +0530

    added some styles in homepage

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,10 +3,13 @@ import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
-        <div>
-            <Link to="/bubble-burst" >Bubble Burst</Link>
-            <Link to="/bounding-box" >Bounding Box</Link>
-        </div>
+        <div className = "main" >
+            <h1>Please select the project you want to see:</h1>
+            <div className="menu">
+                <Link className="button" to="/bubble-burst" >Bubble Burst</Link>
+                <Link className="button" to="/bounding-box" >Bounding Box</Link>
+            </div>
+        </div >
     )
 }
 
